113c65688f07d237dda200143628bb65f37b4609
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-04-08 18:00:18 +0700

    vczx

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ var server = require('http').createServer(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/getname', function(req, res, next) {
+    res.send({data:'ok roi'});
+});
 
 router.post('/fileupload',function (req,res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
+            console.dir(files);
             res.write('File uploaded');
             res.end();
         });

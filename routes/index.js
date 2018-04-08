1d076161f2b8c16b74c1e1b81c7c53d8a62f6387
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-04-08 17:54:44 +0700

    vczx

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,13 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/fileupload',function (req,res) {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-        var oldpath = files.file.path;
-        var newpath =__dirname.substr(0,__dirname.length-6)+ '/uploads/'+files.file.name;
-        fs.rename(oldpath, newpath, function (err) {
-            if (err) throw err;
-            res.write('File uploaded and moved!');
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            res.write('File uploaded');
             res.end();
         });
-    });
+    }
 });
 
 module.exports = router;

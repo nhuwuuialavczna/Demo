cfba8ae1f39219bbd0af5c79db15c76ef43b0745
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-04-08 19:10:52 +0700

    vczx

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,7 @@ router.post('/getname', function (req, res, next) {
 router.post('/fileupload', function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        // res.write(files.file.path+":  " + );
-        copyFile(files.file,'uploads',function () {
-
-        });
-
-        // res.send('<img src="uploads/393c4776ca8deb35a2392c2e33e09a0d.jpg"/>');
+        res.send('<img src="uploads/393c4776ca8deb35a2392c2e33e09a0d.jpg"/><br>'+files.file.length);
         res.end();
     });
 });

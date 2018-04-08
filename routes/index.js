6a51518bf05043fc2642060958ebc6e1cb338ab6
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-04-08 18:36:42 +0700

    vczx

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var formidable =  require('formidable');
+var formidable = require('formidable');
+var copyFile = require('quickly-copy-file');
 var fs = require('fs');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.post('/getname', function(req, res, next) {
-    res.send({data:'ok roi'});
+router.post('/getname', function (req, res, next) {
+    res.send({data: 'ok roi'});
 });
 
-router.post('/fileupload',function (req,res) {
+router.post('/fileupload', function (req, res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
-            res.write('File uploaded: '+files.file.path+"<br>"+__dirname);
+            copyFile(fields.file, __dirname + fields.file, function (error) {
+                if (error) return console.error(error);
+                console.log('File was copied!')
+            });
+
             res.end();
         });
     }
